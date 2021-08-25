c1ac082df3f20aaf399708987cab1b525d09d137
mason.hows11 <mason.hows11@gmail.com>
2021-08-25 19:29:40 +0430

    samples updated

--- resources/views/front/include/main_menu.blade.php
+++ resources/views/front/include/main_menu.blade.php
@@ -4,7 +4,7 @@
         <a href="/" class="col-lg-2 col-md-2 menu_item ">
             <div><p class="text-center mt-4">خانه</p></div>
         </a>
-        <a href="#" class="col-lg-2 col-md-2 menu_item ">
+        <a href="/samples/index" class="col-lg-2 col-md-2 menu_item ">
             <div><p class="text-center mt-4">نمونه کارها</p></div>
         </a>
         <a href="#" class="col-lg-2 col-md-2 menu_item ">
