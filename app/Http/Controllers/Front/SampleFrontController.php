031b13528545f466a11bcf0c40c5f09f913e5f3e
mason.hows11 <mason.hows11@gmail.com>
2021-08-26 18:40:55 +0430

    changes save image code

--- app/Http/Controllers/Front/SampleFrontController.php
+++ app/Http/Controllers/Front/SampleFrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Sample;
 use Illuminate\Http\Request;
 
 class SampleFrontController extends Controller
@@ -13,6 +14,7 @@ class SampleFrontController extends Controller
     public function index()
     {
         $categories = Category::where('parent_id',null)->get();
+        $samples = Sample::all();
         return view('front.samples.samples')
             ->with('categories',$categories);
     }
