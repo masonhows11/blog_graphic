bbf7c72b86b42556771f36404320be00ca265455
mason.hows11 <mason.hows11@gmail.com>
2021-08-27 19:17:51 +0430

    add morilog jalali

--- resources/views/front/samples/sample.blade.php
+++ resources/views/front/samples/sample.blade.php
@@ -9,18 +9,18 @@
                 @include('front.samples.sample_category')
             </div>
 
-            <div class="col-md-9  one-sample" style="border:1px solid tomato">
+            <div class="col-md-9  one-sample">
                 <div class="row d-flex justify-content-center">
-                    <div class="col-lg-9 mt-2" style="border:1px solid tomato">
+                    <div class="col-lg-9 mt-2">
                         <div class="card">
                             <img src="{{ asset('/template/samples/'.$sample->main_image) }}" class="card-img-top" alt="...">
                             <div class="card-header">
-                                Featured
+                                {{$sample->title}}
                             </div>
                             <div class="card-body">
-                                <h5 class="card-title">Special title treatment</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                <p class="card-text">{{ strip_tags($sample->description) }}</p>
+{{--                                <a href="#" class="btn btn-primary">Go somewhere</a>--}}
                             </div>
                         </div>
                     </div>
