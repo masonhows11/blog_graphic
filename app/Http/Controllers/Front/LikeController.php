ddc2b24b5a17e3f5847ca31f1746880b16a19b72
mason.hows11 <mason.hows11@gmail.com>
2021-09-01 19:22:00 +0430

    like backend code updated 80%

--- app/Http/Controllers/Front/LikeController.php
+++ app/Http/Controllers/Front/LikeController.php
@@ -14,34 +14,38 @@ class LikeController extends Controller
 
     public function updateLike(Request $request)
     {
-         //return $request;
+
 
         $sample_id = $request->sample_id;
         $is_like = $request['is_like'] === 'true';
-        //$update_like = false;
+
 
         $user_id = Auth::id();
         $sample = Sample::find($sample_id);
 
-        if(!$sample)
-        {
+        if (!$sample) {
             return null;
         }
 
-        $like_exists = Like::where('sample_id','=',$sample_id)->where('user_id','=',$user_id)->first();
+        $like_exists = Like::where('sample_id', '=', $sample_id)->where('user_id', '=', $user_id)->first();
 
-        if($like_exists)
-        {
 
-          $already_like = $like_exists->like;
+       if ($like_exists) {
 
-            if($is_like == $already_like)
-            {
+            $already_like = $like_exists->like;
+
+            if ($already_like == $is_like) {
                 $like_exists->delete();
+                return null;
+            }elseif($already_like != $is_like){
+                $like_exists->like = $is_like;
+                $like_exists->save();
+                return null;
             }
 
 
-        }else {
+
+        } else {
 
             $like = new Like();
             $like->user_id = $user_id;
@@ -52,15 +56,14 @@ class LikeController extends Controller
         }
 
 
-
     }
 
     public function likeCount(Request $request)
     {
 
-        $like_count = Like::where('sample_id',$request->sample_id)->where('like','=',1)->count();
-        $like_dis_count = Like::where('sample_id',$request->sample_id)->where('like','=',0)->count();
-        return response()->json(['like_count'=>$like_count,'like_dis_count'=>$like_dis_count]);
+        $like_count = Like::where('sample_id', $request->sample_id)->where('like', '=', 1)->count();
+        $like_dis_count = Like::where('sample_id', $request->sample_id)->where('like', '=', 0)->count();
+        return response()->json(['like_count' => $like_count, 'like_dis_count' => $like_dis_count]);
     }
 
 
