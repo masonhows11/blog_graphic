e8b19c3868756e23a7164e8808b010228f78be45
mason.hows11 <mason.hows11@gmail.com>
2021-09-06 17:38:45 +0430

    get samples comment with user add comment

--- app/Http/Controllers/Admin/CommentAdController.php
+++ app/Http/Controllers/Admin/CommentAdController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use App\Models\Sample;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CommentAdController extends Controller
 {
     //
     public function index()
     {
-        
+
+        $sample_comments = DB::table('comments')
+            ->join('samples','comments.sample_id','=','samples.id')
+            ->join('users','users.id','=','comments.user_id')
+            ->select('comments.id','samples.title','users.user_name','comments.description')->get();
+       // return  $sample_comments;
+
+
         return view('admin.comment_management.index');
     }
 
