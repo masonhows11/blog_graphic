4a9f9149ee9358c9ae79dce0c275edc6f251fc92
mason.hows11 <mason.hows11@gmail.com>
2021-09-10 10:22:04 +0430

    edit tip admin fixed 90%

--- resources/views/admin/tip_management/edit.blade.php
+++ resources/views/admin/tip_management/edit.blade.php
@@ -37,7 +37,7 @@
                     @enderror
                 </div>
                 <div class="input-group">
-                    <input type="text" id="image_label" class="form-control @error('image') is-invalid @enderror"
+                    <input type="text" id="image_title" class="form-control @error('image') is-invalid @enderror"
                            name="image"
                            aria-label="Image"
                            aria-describedby="button-image" value="{{ $tip->image }}">
@@ -58,4 +58,26 @@
 
     </div>
 @endsection
+@section('my_script_admin')
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            // for button-image-title
+            document.getElementById('button-image').addEventListener('click', (event) => {
+                event.preventDefault();
+                inputId = 'image_title'
+                window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+            });
 
+        });
+        function fmSetLink($url) {
+            document.getElementById(inputId).value = $url;
+        }
+    </script>
+    <script type="text/javascript" src="{{ asset('admin/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('editor-text', {
+            language: 'fa',
+            removePlugins: 'image',
+        });
+    </script>
+@endsection
